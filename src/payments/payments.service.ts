41ab1d463df600c047e609a2bbbf6234f06efe82
mozadev <ceosmore@gmail.com>
2024-08-05 21:25:24 -0500

    chore: Add initial project configuration files and setup Dto payment-session.dto, payment-controller (config webhook),  createPaymentSession( get lineItems), create session stripe that include line_ites, orderId as metada, mode : 'payment', success_url, cance_url. Config Stripe webhook using hookdeck as manager of  notifications of pay from stripe to backend and switch case 'charge.succeded'

---
+++ src/payments/payments.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@nestjs/common';
+import { envs } from 'src/config';
+import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class PaymentsService {
+
+    private readonly stripe = new Stripe(envs.stripeSecret);
+
+    async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+
+        // with implicit return 
+        const { currency, items, orderId } = paymentSessionDto;
+
+        const lineItems = items.map(item => ({
+            price_data: {
+
+                currency: currency,
+                product_data: {
+                    name: item.name
+                },
+                unit_amount: Math.round(item.price * 100),
+            },
+            quantity: item.quantity
+        }))
+
+        const session = await this.stripe.checkout.sessions.create({
+            // place here id of my order
+            payment_intent_data: {
+                metadata: {
+                    orderId: orderId
+                }
+            },
+            line_items: lineItems,
+            mode: 'payment',
+            success_url: 'http://localhost:3003/payments/success',
+            cancel_url: 'http://localhost:3003/payments/cancel'
+        });
+
+        return session;
+    }
+
+
+    // the stripe's sign will come from the petition's header
+    async stripeWebhook(req: Request, res: Response) {
+        const sig = req.headers['stripe-signature'];
+
+        let event: Stripe.Event;
+        //This is your Stripe CLI webhook secret for testing your endpoint locally.
+        //const endpointSecret ='whsec_3ed5e120249e2fbf279696c26b47e9aa08844764fc5daedfdb2e6d8aad2bba4a';
+
+        // Real
+        const endpointSecret = 'whsec_ywoVtvi5GXLW8cw6gJzgsjWUQuY9cnWu'
+
+
+        try {
+            event = this.stripe.webhooks.constructEvent(
+                //req.body, in express
+                req['rawBody'], // in nest , check main.ts 
+                sig,
+                endpointSecret,
+            );
+        } catch (err) {
+            res.status(400).send(`Webhook Error: ${err.message}`);
+            return; // this is important for it not continue
+        }
+
+        // console.log({ event });
+        switch (event.type) {
+            case 'charge.succeeded':
+                const chargeSucceeded = event.data.object;
+                //TODO: Call our microservice
+                console.log({
+                    metadata: chargeSucceeded.metadata,
+                    orderId: chargeSucceeded.metadata.orderId,
+                });
+                break;
+            default:
+                console.log(`Event ${event.type} not handled`)
+
+        }
+
+        //return res.status(200).json({ sig })
+        return res.status(200).json({ sig })
+
+    }
+
+}
